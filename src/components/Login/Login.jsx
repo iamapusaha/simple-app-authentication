db1a28b1d04bbb2f04ba2702a7bf469b87f949dd
iamapusaha <apusaha392@gmail.com>
2023-09-28 16:15:38 +0600

    added gitHub signIn

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,24 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { useState } from "react";
 const Login = () => {
     const [user, setUser] = useState()
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
     const handleLogin = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const logInUser = result.user;
+                console.log(logInUser);
+                setUser(logInUser)
+            })
+            .catch(error => {
+                console.log(error, error.message);
+            })
+    }
+    const handleGitHubSignIn = () => {
+        signInWithPopup(auth, githubProvider)
             .then(result => {
                 const logInUser = result.user;
                 console.log(logInUser);
@@ -30,7 +42,10 @@ const Login = () => {
         <div>
             {user ?
                 <button className="bg-orange-200" onClick={handleLogOut}>Log Out</button>
-                : <button className="bg-orange-200" onClick={handleLogin}>Login with google</button>
+                : <div className="flex gap-4">
+                    <button className="bg-orange-200" onClick={handleLogin}>Login with google</button>
+                    <button className="bg-orange-200" onClick={handleGitHubSignIn}>Login with github</button>
+                </div>
 
             }
 
