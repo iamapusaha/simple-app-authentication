e3426ba23413c058e10a1ed12461276d74c7c95c
iamapusaha <apusaha392@gmail.com>
2023-09-28 12:28:28 +0600

    project setup done

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBFXmi0kARtgZgQZFWluJH1xwgr0tsAcdw",
+    authDomain: "simple-firebase-cb311.firebaseapp.com",
+    projectId: "simple-firebase-cb311",
+    storageBucket: "simple-firebase-cb311.appspot.com",
+    messagingSenderId: "679336860776",
+    appId: "1:679336860776:web:b55a388eb165599c5d6fdd"
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
