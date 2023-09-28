990da15de83191f4159923f542c054ac15e9b2d2
iamapusaha <apusaha392@gmail.com>
2023-09-28 15:31:53 +0600

    added logout formula

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { useState } from "react";
 const Login = () => {
@@ -16,9 +16,24 @@ const Login = () => {
                 console.log(error, error.message);
             })
     }
+    const handleLogOut = () => {
+        const auth = getAuth();
+        signOut(auth)
+            .then(() => {
+                setUser(null)
+            }).catch((error) => {
+                console.log(error, error.message);
+            });
+
+    }
     return (
         <div>
-            <button className="bg-orange-200" onClick={handleLogin}>Login with google</button>
+            {user ?
+                <button className="bg-orange-200" onClick={handleLogOut}>Log Out</button>
+                : <button className="bg-orange-200" onClick={handleLogin}>Login with google</button>
+
+            }
+
             {user && <div>
                 <h1>user: {user.displayName}</h1>
                 <p>{user.email}</p>
